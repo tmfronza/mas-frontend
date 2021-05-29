329d83fd93cdc9ecb8b85ea014f392e75ad60d24
Thiago Monteiro Fronza <tm.fronza@gmail.com>
2021-05-29 16:00:05 -0300

    Controls of forms and modals.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,44 @@
 import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { Button } from '../../components/Button';
-import { Container, Content, FormContainer, InputContainer, Background } from './styles';
+import { Container, Content, FormContainer, InputContainer, Error, Background } from './styles';
+
+interface FormData {
+    email: string;
+    password: string;
+}
 
 export function Login() {
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = handleSubmit(data => alert(JSON.stringify(data)));
+
     return (
         <Container>
             <Content>
                 <FormContainer>
                     <h2>Faça seu login</h2>
-                    <form action=''>
+                    <form onSubmit={onSubmit}>
                         <InputContainer>
                             <FiMail size={40} />
                             <input 
                                 type='email' 
                                 placeholder='E-mail'
+                                {... register('email', {required: true})}
                             />
                         </InputContainer>
+                        {errors.email && <Error>Preenchimento do campo é obrigatório.</Error>}
                         <InputContainer>
                             <FiLock size={40} />
                             <input 
                                 type='password' 
                                 placeholder='Senha'
+                                {... register('password', {required: true})}
                             />
                         </InputContainer>
+                        {errors.password && <Error>Preenchimento do campo é obrigatório.</Error>}
                         <Button type='submit'>Entrar</Button>
                     </form>
                     <Link to='/register'>
